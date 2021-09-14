18ad60f2c3dfd10c56d7349977abc6ed6a48e1a7
ray-de-lara <raydelara99@gmail.com>
2021-09-13 19:59:27 -0500

    Bonito Contador

---
+++ script.js
@@ -0,0 +1,31 @@
+const printDays = document.getElementById("day");
+const printHours = document.getElementById("hour");
+const printMinutes = document.getElementById("minute");
+const printSeconds = document.getElementById("second");
+
+const newYears = '01 01 2022';
+
+function countdown(){
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = new Date(newYearsDate - currentDate)/1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 60) %60;
+    const seconds = Math.floor(totalSeconds %60);
+
+    printDays.innerHTML = (days);
+    printHours.innerHTML = (hours);
+    printMinutes.innerHTML = (minutes);
+    printSeconds.innerHTML = (seconds);
+
+
+    console.log(days, hours, minutes, seconds);
+
+
+}
+countdown();
+
+setInterval(countdown, 1000);
